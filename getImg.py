508fd8d02e8b4c4f5944594d15e4332be907b863
TTY <cbb111232@nptu.edu.tw>
2025-01-19 15:19:13 +0800

    first commit

---
+++ getImg.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.klook.com/zh-TW/activity/8875-chimei-museum-permanent-exhibition-ticket-tainan/'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1'
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.prettify())
